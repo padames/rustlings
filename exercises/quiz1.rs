83597f0152121b1cfca317a31d322724094bf072
padames <pablo.adames@gmail.com>
2024-02-25 22:56:01 -0700

    Primitive_types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num_apples: u32) ->i32{
+    let price :i32 = num_apples as i32;
+    if num_apples <= 40
+    {
+           return price * 2;
+    }
+    else {
+        return price;
+    }
+}
 
 // Don't modify this function!
 #[test]
