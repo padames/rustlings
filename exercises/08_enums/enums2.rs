a5b4bb9270e3f5617ed67eede32ed09c524234ce
pablo@XP13 <pablo.adames@gmail.com>
2024-08-16 22:45:30 -0600

    undoing changes too convoluted enums2

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -3,28 +3,26 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
+// I AM NOT DONE
+
 #[derive(Debug)]
-enum Message<'a> {
-     Move{x: u32, y: u32},
-     Echo(&'a str),
-     ChangeColor(u8,u8,u8),
-     Quit,           
+enum Message {
+    // TODO: define the different variants used below
 }
 
-impl Message<'_> {
+impl Message {
     fn call(&self) {
         println!("{:?}", self);
     }
 }
 
 fn main() {
-    let var_name = [
+    let messages = [
         Message::Move { x: 10, y: 30 },
-        Message::Echo(&String::from("hello world")),
+        Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
         Message::Quit,
     ];
-    let messages = var_name;
 
     for message in &messages {
         message.call();
