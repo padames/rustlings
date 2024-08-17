a53b97e33749f20f7f381a845ca03093aa9503c6
pablo@XP13 <pablo.adames@gmail.com>
2024-08-16 21:54:26 -0600

    Aug 16, 2024 enum2 unfinished

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -3,26 +3,28 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
-enum Message {
-    // TODO: define the different variants used below
+enum Message<'a> {
+     Move{x: u32, y: u32},
+     Echo(&'a str),
+     ChangeColor(u8,u8,u8),
+     Quit,           
 }
 
-impl Message {
+impl Message<'_> {
     fn call(&self) {
         println!("{:?}", self);
     }
 }
 
 fn main() {
-    let messages = [
+    let var_name = [
         Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
+        Message::Echo(&String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
         Message::Quit,
     ];
+    let messages = var_name;
 
     for message in &messages {
         message.call();
